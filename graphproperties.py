aa738187f3e47f0bf5761761403ba11123a1351d
Nico Van Cleemput <nico.vancleemput@gmail.com>
2014-09-09 20:11:41 +0000

    Fixed bug in is_lindquester and is_complete: distances not indexed by their position

--- graphproperties.py
+++ graphproperties.py
@@ -126,7 +126,7 @@ def is_lindquester(g):
     V = g.vertices()
     for i in range(n):
         for j in range(n):
-            if i > j and D[i][j] == 2:
+            if i > j and D[V[i]][V[j]] == 2:
                 if len(neighbors_set(g,[V[i],V[j]])) < (2*n-1)/3.0:
                     return False
     return True
@@ -140,7 +140,7 @@ def is_complete(g):
     D = g.distance_all_pairs()
     for i in range(n):
         for j in range(i):
-            if D[i][j] != 1:
+            if D[V[i]][V[j]] != 1:
                 return False
     return True
 
