b8ee3040666eef02f4b8c54a9c85782326b18b16
Nico Van Cleemput <nico.vancleemput@gmail.com>
2014-09-09 20:14:31 +0000

    Removed unnecessary check

--- graphproperties.py
+++ graphproperties.py
@@ -125,8 +125,8 @@ def is_lindquester(g):
     n = g.order()
     V = g.vertices()
     for i in range(n):
-        for j in range(n):
-            if i > j and D[V[i]][V[j]] == 2:
+        for j in range(i):
+            if D[V[i]][V[j]] == 2:
                 if len(neighbors_set(g,[V[i],V[j]])) < (2*n-1)/3.0:
                     return False
     return True
