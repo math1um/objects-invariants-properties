04f983c625e226a19c2f68d99e613b79fbe3cd6f
Craig Larson <clarson@vcu.edu>
2014-09-24 20:07:24 +0000

    added bipartite_chromatic invariant to gt.py

--- gt.py
+++ gt.py
@@ -275,6 +275,14 @@ def average_vertex_temperature(g):
      n = g.order()
      return sum([D[i]/(n-D[i]+0.0) for i in range(n)])/n
 
+#a solution of the invariant interpolation problem for upper bound of chromatic number for c8chords
+#all upper bounds in theory have value at least 3 for c8chords
+#returns 2 for bipartite graphs, order for non-bipartite
+def bipartite_chromatic(g):
+    if g.is_bipartite():
+        return True
+    else return g.order()
+
 efficiently_computable_invariants = [Graph.average_distance, Graph.diameter, Graph.radius, Graph.girth,  Graph.order, Graph.size, Graph.szeged_index, Graph.wiener_index, min_degree, max_degree, Graph.average_degree, matching_number, residue, annihilation_number, fractional_alpha, lovasz_theta, cvetkovic, cycle_space_dimension, card_center, card_periphery, max_eigenvalue, kirchhoff_index, largest_singular_value, Graph.vertex_connectivity, Graph.edge_connectivity, Graph.maximum_average_degree, Graph.density, welsh_powell, wilf, brooks, different_degrees, szekeres_wilf, average_vertex_temperature]
 
 def median_degree(g):
