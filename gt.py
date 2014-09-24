bac38b9ee98d836e3d0952f0ae6bbfd1d72b1173
Craig Larson <clarson@vcu.edu>
2014-09-24 18:25:13 +0000

    added invariant average_vertex_temperature to gt.py

--- gt.py
+++ gt.py
@@ -270,7 +270,12 @@ def szekeres_wilf(g):
         if gc.size() == 0:
             return i + 1
 
-efficiently_computable_invariants = [Graph.average_distance, Graph.diameter, Graph.radius, Graph.girth,  Graph.order, Graph.size, Graph.szeged_index, Graph.wiener_index, min_degree, max_degree, Graph.average_degree, matching_number, residue, annihilation_number, fractional_alpha, lovasz_theta, cvetkovic, cycle_space_dimension, card_center, card_periphery, max_eigenvalue, kirchhoff_index, largest_singular_value, Graph.vertex_connectivity, Graph.edge_connectivity, Graph.maximum_average_degree, Graph.density, welsh_powell, wilf, brooks, different_degrees, szekeres_wilf]
+def average_vertex_temperature(g):
+     D = g.degree()
+     n = g.order()
+     return sum([D[i]/(n-D[i]+0.0) for i in range(n)])/n
+
+efficiently_computable_invariants = [Graph.average_distance, Graph.diameter, Graph.radius, Graph.girth,  Graph.order, Graph.size, Graph.szeged_index, Graph.wiener_index, min_degree, max_degree, Graph.average_degree, matching_number, residue, annihilation_number, fractional_alpha, lovasz_theta, cvetkovic, cycle_space_dimension, card_center, card_periphery, max_eigenvalue, kirchhoff_index, largest_singular_value, Graph.vertex_connectivity, Graph.edge_connectivity, Graph.maximum_average_degree, Graph.density, welsh_powell, wilf, brooks, different_degrees, szekeres_wilf, average_vertex_temperature]
 
 def median_degree(g):
     return median(g.degree())
