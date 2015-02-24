a523265b622ded86505844c6fa1cef4b9a9915d1
Craig Larson <clarson@vcu.edu>
2015-02-24 21:28:54 +0000

    added is_pebbling_class0

--- gt.py
+++ gt.py
@@ -1090,6 +1090,14 @@ def remove_twins(g):
     #print Verts
     return g.subgraph(Verts)
 
+#can't compute membership in this class directly. instead testing isomorhism for 400 known class0 graphs
+def is_pebbling_class0(g):
+    for hkey in class0graphs_dict:
+        h = Graph(class0graphs_dict[hkey])
+        if g.is_isomorphic(h):
+            return True
+    return False
+
 
 def localise(f):
     """
@@ -1560,6 +1568,26 @@ c5.name(new = "c5")
 graph_objects = [paw, kite, p4, dart, k3, k4, k5, c6ee, c5chord, graphs.DodecahedralGraph(), c8chorded, c8chords, graphs.ClebschGraph(),  prismy, c24, c26, c60, c6xc6, holton_mckay, sixfour, c4, graphs.PetersenGraph(), p2, graphs.TutteGraph(), non_ham_over, throwing, throwing2, throwing3, kratsch_lehel_muller, graphs.BlanusaFirstSnarkGraph(), graphs.BlanusaSecondSnarkGraph(), graphs.FlowerSnark(), s3, ryan3, k10, graphs.MycielskiGraph(5), c3mycieski, c3mycielski4, alon_seymour, s13e, ryan2, s22e, difficult11, graphs.BullGraph(), graphs.ChvatalGraph(), graphs.ClawGraph(), graphs.DesarguesGraph(), graphs.DiamondGraph(), graphs.FlowerSnark(), graphs.FruchtGraph(), graphs.HoffmanSingletonGraph(), graphs.HouseGraph(), graphs.HouseXGraph(), graphs.OctahedralGraph(), graphs.ThomsenGraph(), graphs.TetrahedralGraph(), pete , graphs.PappusGraph(), graphs.GrotzschGraph(), graphs.GrayGraph(), graphs.HeawoodGraph(), graphs.HerschelGraph(), graphs.SchlaefliGraph(), graphs.CoxeterGraph(), graphs.BrinkmannGraph(), graphs.TutteCoxeterGraph(), graphs.TutteGraph(), graphs.RobertsonGraph(), graphs.FolkmanGraph(), graphs.Balaban10Cage(), graphs.PappusGraph(), graphs.TietzeGraph(), graphs.SylvesterGraph(), graphs.SzekeresSnarkGraph(), graphs.MoebiusKantorGraph(), ryan, inp, c4c4, regular_non_trans, bridge, p10k4, c100, starfish, c5k3, k5pendant, graphs.ShrikhandeGraph(), graphs.MeredithGraph(), sylvester, fork, edge_critical_5, edge_critical_11_1, edge_critical_11_2, pete_minus, c5] + alpha_critical_graphs + chromatic_index_critical_7
 
 chromatic_index_critical_graphs = chromatic_index_critical_7 + [edge_critical_5, edge_critical_11_1, edge_critical_11_2, pete_minus]
+
+#class 0 pebbling graphs
+import pickle, os, os.path
+class0graphs_dict = pickle.load(open(os.getcwd() + "/conjecturing/objects-invariants-properties/class0graphs_dictionary.pickle","r"))
+class0graphs = []
+for d in class0graphs_dict:
+    g = Graph(class0graphs_dict[d])
+    g.name(new = d)
+    class0graphs.append(g)
+class0small = [g for g in class0graphs if g.order() < 30]
+
+#class 0 pebbling graphs
+import pickle, os, os.path
+class0graphs_dict = pickle.load(open(os.getcwd() + "/conjecturing/objects-invariants-properties/class0graphs_dictionary.pickle","r"))
+class0graphs = []
+for d in class0graphs_dict:
+    g = Graph(class0graphs_dict[d])
+    g.name(new = d)
+    class0graphs.append(g)
+class0small = [g for g in class0graphs if g.order() < 30]
 
 #class 0 pebbling graphs
 import pickle, os, os.path
