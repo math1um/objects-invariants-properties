math1um/objects-invariants-properties

commit 3b6bef3432ba60becfd38ab445b70f76857b77d5

Speedup pairs_have_unique_common_neighbor
Changed implementation to avoid doing the same calculation twice. Copied and modified code from max/min_common_neighbors to perform both at once, so we don't call them both unnecessarily.
Issue #523

src/Properties/properties.sage
@@ -24,24 +24,25 @@ def is_complement_of_chordal(g):
     h = g.complement()
     return h.is_chordal()
 
-#a consequence of the Friendship Theorem:
-#the only connected graphs where every pair of vertices has a unique neghbor are flowers
 def pairs_have_unique_common_neighbor(g):
     """
-    tests if a graph is a collection of disjoint triangles with a single identified vertex
-        sage: pairs_have_unique_common_neighbor(flower(5))
-        True
-        sage: pairs_have_unique_common_neighbor(k3)
-        True
-        sage: pairs_have_unique_common_neighbor(k4)
-        False
+    True if each pair of vertices in g has exactly one common neighbor
+
+    Related to the Friendship Theorem: the only connected graphs where every pair of vertices 
+    has a unique neghbor are flowers.
+
+    sage: pairs_have_unique_common_neighbor(flower(5))
+    True
+    sage: pairs_have_unique_common_neighbor(k3)
+    True
+    sage: pairs_have_unique_common_neighbor(k4)
+    False
     """
-    if max_common_neighbors(g) != 1:
-        return False
-    elif min_common_neighbors(g) != 1:
-        return False
-    else:
-        return True
+    from itertools import combinations
+    for (u,v) in combinations(g.vertices(), 2):
+        if len(common_neighbors(g, V[i], V[j])) != 1:
+            return False
+    return max
     
 def is_distance_transitive(g):
     """

commit d2a96a712d5414e46d9b4e6435373f3c130f920a

Fix pairs_have_unique_common_neighbor
Corrected references to wrong parameters and fixed incorrect return statement introduced in 3b6bef3.
Issue #529

src/Properties/properties.sage
@@ -40,9 +40,9 @@ def pairs_have_unique_common_neighbor(g):
     """
     from itertools import combinations
     for (u,v) in combinations(g.vertices(), 2):
-        if len(common_neighbors(g, V[i], V[j])) != 1:
+        if len(common_neighbors(g, u, v)) != 1:
             return False
-    return max
+    return True
 
 def is_distance_transitive(g):
     """