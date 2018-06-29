565d5b3d0b5b4909881670f3995f1cd8fc4b7a6f
yirkajk <yirkajk@vcu.edu>
2018-06-29 10:48:36 -0400

    Compute some efficient props[89:]

---
+++ db/properties/is_circumscribable.sql
@@ -0,0 +1,4 @@
+INSERT INTO "prop_values" VALUES('is_circumscribable','JIQCC?ZJ`i?',1);
+INSERT INTO "prop_values" VALUES('is_circumscribable','KBD@IO_`SDPC',1);
+INSERT INTO "prop_values" VALUES('is_circumscribable','KIKY@?B_[GOP',1);
+INSERT INTO "prop_values" VALUES('is_circumscribable','KJOGOga_[GOP',1);
